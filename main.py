486b774b7d876e60b63615495b3acee6a7051a0c
hyperpulsor <rakanfasya@gmail.com>
2024-09-30 09:29:56 +0700

    Feat: Implemented year, quarter, and PJP's name filter to df

--- main.py
+++ main.py
@@ -6,14 +6,38 @@ from visualize import *
 
 def main():
     # Initial Web Setup
-    set_page_settings()
+    col = set_page_settings()
     set_data_settings()
+
+    with col[0]:
+        st.text("Column 1")
+    with col[1]:
+        st.text("Column 2")
+    with col[2]:
+        st.text("Column 3")
+
     uploaded_file = st.file_uploader("Choose a Excel file")
     df = None
     if uploaded_file is not None:
-        df = pd.read_excel(uploaded_file, 'Trx_PJPJKT')
-        df = preprocess_data(df)
-        make_pie_chart(df, 5)
+        df = load_data(uploaded_file)
+
+        pjps = ['All'] + list(df['Nama PJP'].unique())
+        selected_pjp = st.sidebar.selectbox('Select PJP:', pjps)
+        quarter = ['All'] + list(df['Quarter'].unique())
+        selected_quarter = st.sidebar.selectbox('Select Quarter:', quarter)
+        year = ['All'] + list(df['Year'].unique())
+        selected_year = st.sidebar.selectbox('Select Year:', year)
+
+        df_preprocessed = preprocess_data(df)
+        df_preprocessed_time = preprocess_data_time(df)
+
+        make_pie_chart(df_preprocessed, 5)
+
+        filtered_data = filter_data(df=df_preprocessed_time,
+                                    selected_pjp=selected_pjp,
+                                    selected_quarter=selected_quarter,
+                                    selected_year=selected_year)
+        st.dataframe(filtered_data)
     else:
         st.warning("You Must Upload a CSV or Excel File")
 
--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,20 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
+@st.cache_data
+def load_data(uploaded_file):
+    df = pd.read_excel(uploaded_file, 'Trx_PJPJKT')
+    return df
+
+def filter_data(df, selected_pjp, selected_year, selected_quarter):
+    if selected_pjp != 'All':
+        df = df[df['Nama PJP'] == selected_pjp]
+    if selected_year != 'All':
+        df = df[df['Year'] == selected_year]
+    if selected_quarter != 'All':
+        df = df[df['Quarter'] == selected_quarter]
+    return df
+
 def set_data_settings():
     pd.set_option('display.float_format', '{:,.2f}'.format)
 
@@ -12,11 +26,10 @@ def set_page_settings():
         layout="wide",
         initial_sidebar_state="expanded")
     st.title('Data LTDBB PJP LR JKT Visualization')
+    col = st.columns((1.5, 4.5, 2), gap='medium')
     with st.sidebar:
         st.image("./static/Logo.png")
-        st.title('Data Analytics')
-        color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
-        selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+    return col
 
 def preprocess_data(df):
     df = df.drop(columns=['Nama PJP Conv Final'])
@@ -29,4 +42,20 @@ def preprocess_data(df):
 
     total_sum_of_nom = df['Sum of Total Nom'].sum()
     df['Market Share (%)'] = (df['Sum of Total Nom'] / total_sum_of_nom) * 100
+    return df
+
+def preprocess_data_time(df):
+    df = df.drop(columns=['Nama PJP Conv Final'])
+    df = df.groupby(['Nama PJP', 'Year', 'Quarter']).agg({'Fin Jumlah Inc':'sum', 'Fin Nilai Inc':'sum',
+                                                          'Fin Jumlah Out':'sum', 'Fin Nilai Out':'sum',
+                                                          'Fin Jumlah Dom':'sum', 'Fin Nilai Dom':'sum'})
+    
+    df = df.rename(columns=lambda x: 'Sum of ' + x)
+    df = df.reset_index()
+    
+    df['Sum of Total Nom'] = df['Sum of Fin Nilai Inc'] + df['Sum of Fin Nilai Out'] + df['Sum of Fin Nilai Dom']
+    
+    total_sum_of_nom = df.groupby(['Year', 'Quarter'])['Sum of Total Nom'].transform('sum')
+    
+    df['Market Share (%)'] = ((df['Sum of Total Nom'] / total_sum_of_nom) * 100).round(2)
     return df
